86bd80b346eebd0228d138f127c2c9c4d61200b9
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-09-08 13:21:17 -0300

    Project files

---
+++ SeekAndPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeekAndPlay
+//
+//  Created by Nicolás Fernández Alarcón on 08-09-18.
+//  Copyright © 2018 Nicolás Fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
