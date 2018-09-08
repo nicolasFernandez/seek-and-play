bd9d8f711eb8f4f845404ea1f65b08f6873bdba6
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-09-08 14:02:34 -0300

    Added base view controllers

---
+++ SeekAndPlay/ViewControllers/SearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewController.swift
+//  SeekAndPlay
+//
+//  Created by Nicolás Fernández Alarcón on 08-09-18.
+//  Copyright © 2018 Nicolás Fernández. All rights reserved.
+//
+
+import Foundation
