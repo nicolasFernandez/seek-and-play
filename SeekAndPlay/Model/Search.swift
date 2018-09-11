fdad6c529efa7f350cc9d19396d7a1a27921a3a4
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-09-11 09:17:48 -0300

    Added iTunes album lookup

--- SeekAndPlay/Model/Search.swift
+++ SeekAndPlay/Model/Search.swift
@@ -13,22 +13,21 @@ class Search {
 	
 	var term = String()
 	let limit = 20
-
+	
 	func searchTerm(_ term: String) {
 		//Note: URL encoding replaces spaces with the plus (+) character and all characters except the following are encoded: letters, numbers, periods (.), dashes (-), underscores (_), and asterisks (*)
 		if let encoded = term.addingPercentEncoding(withAllowedCharacters: .alphanumerics) {
 			
 			//TODO: save term in database for recentSearches
 			
-			//TODO: check how to look for a collection entity=music&attribute=albumTerm // CollectionName
 			Alamofire.request("https://itunes.apple.com/search?term=\(encoded)&media=music&limit=\(limit)").responseJSON { response in
 				print("Request: \(String(describing: response.request))")   // original url request
 				print("Response: \(String(describing: response.response))") // http url response
 				print("Result: \(response.result)")                         // response serialization result
 				
-
+				
 				if let json = response.result.value {
- 					print("JSON: \(json)") // serialized json response
+					print("JSON: \(json)") // serialized json response
 					//TODO: trackName
 					//TODO: artistName | collectionName
 					//collectionId, collectionViewUrl, artworkUrl100,  previewUrl
@@ -41,6 +40,29 @@ class Search {
 		} else {
 			print("empty encoded term")
 		}
+	}
+	
+	func getAlbumResults(_ id: Int) {
+		//Note: URL encoding replaces spaces with the plus (+) character and all characters except the following are encoded: letters, numbers, periods (.), dashes (-), underscores (_), and asterisks (*)
+		
+		Alamofire.request("https://itunes.apple.com/lookup?id=\(id)&entity=song").responseJSON { response in
+			print("Request: \(String(describing: response.request))")   // original url request
+			print("Response: \(String(describing: response.response))") // http url response
+			print("Result: \(response.result)")                         // response serialization result
+			
+			
+			if let json = response.result.value {
+				print("JSON: \(json)") // serialized json response
+				//TODO: trackName
+				//TODO: artistName | collectionName
+				//collectionId, collectionViewUrl, artworkUrl100,  previewUrl
+			}
+			
+			if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+				print("Data: \(utf8Text)") // original server data as UTF8 string
+			}
+		}
+		
 	}
 	
 	//TODO: implement search methods
