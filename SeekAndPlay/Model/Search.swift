ffa308766f504a084e47662ea700ced4619e54e5
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-09-11 08:57:19 -0300

    Implemented base search message

--- SeekAndPlay/Model/Search.swift
+++ SeekAndPlay/Model/Search.swift
@@ -7,10 +7,39 @@
 //
 
 import Foundation
+import Alamofire
 
 class Search {
 	
 	var term = String()
+	let limit = 20
+
+	func searchTerm(_ term: String) {
+		//Note: URL encoding replaces spaces with the plus (+) character and all characters except the following are encoded: letters, numbers, periods (.), dashes (-), underscores (_), and asterisks (*)
+		if let encoded = term.addingPercentEncoding(withAllowedCharacters: .alphanumerics) {
+			
+			//TODO: check how to look for a collection entity=music&attribute=albumTerm // CollectionName
+			Alamofire.request("https://itunes.apple.com/search?term=\(encoded)&media=music&limit=\(limit)").responseJSON { response in
+				print("Request: \(String(describing: response.request))")   // original url request
+				print("Response: \(String(describing: response.response))") // http url response
+				print("Result: \(response.result)")                         // response serialization result
+				
+
+				if let json = response.result.value {
+ 					print("JSON: \(json)") // serialized json response
+					//TODO: trackName
+					//TODO: artistName | collectionName
+					//collectionId, collectionViewUrl, artworkUrl100,  previewUrl
+				}
+				
+				if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+					print("Data: \(utf8Text)") // original server data as UTF8 string
+				}
+			}
+		} else {
+			print("empty encoded term")
+		}
+	}
 	
 	//TODO: implement search methods
 }
