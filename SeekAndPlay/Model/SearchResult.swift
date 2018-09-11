77ffbe49a0d29057a7a5a8331a3d241b6ac075b5
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-09-11 08:56:32 -0300

    Added search result structure for itunes

---
+++ SeekAndPlay/Model/SearchResult.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResult.swift
+//  SeekAndPlay
+//
+//  Created by Nicolás Fernández Alarcón on 11-09-18.
+//  Copyright © 2018 Nicolás Fernández. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult {
+	//Display data
+	var trackName = String()
+	var artistName = String()
+	var collectionName = String()
+	
+	var collectionId = Int()
+	var collectionViewUrl = String()
+	var artworkUrl100 = String()
+	var previewUrl = String()
+}
