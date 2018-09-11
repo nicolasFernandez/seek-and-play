bffd6e800ab69310409b694a4e32bac160c9bd05
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-09-11 06:40:20 -0300

    Added few model objects

---
+++ SeekAndPlay/Model/History.swift
@@ -0,0 +1,16 @@
+//
+//  History.swift
+//  SeekAndPlay
+//
+//  Created by Nicolás Fernández Alarcón on 11-09-18.
+//  Copyright © 2018 Nicolás Fernández. All rights reserved.
+//
+
+import Foundation
+
+struct History {
+	
+	static var identifier: Int
+	var term: String
+	
+}
---
+++ SeekAndPlay/Model/Search.swift
@@ -0,0 +1,16 @@
+//
+//  Search.swift
+//  SeekAndPlay
+//
+//  Created by Nicolás Fernández Alarcón on 11-09-18.
+//  Copyright © 2018 Nicolás Fernández. All rights reserved.
+//
+
+import Foundation
+
+class Search {
+	
+	var term = String()
+	
+	//TODO: implement search methods
+}
